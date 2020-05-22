38f7ec9e5ef7aaaefc401dcaa998822dad8e0ff8
Julkarnain Shahid <jshahid1997@gmail.com>
2020-05-23 00:59:00 +0530

    Edit Details done

--- src/js/EditDetails.js
+++ src/js/EditDetails.js
@@ -1,5 +1,20 @@
 import React, { Component, Fragment } from "react";
-import { Card, CardBody, Button } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  Button,
+  CardHeader,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Row,
+  Col,
+  Input,
+} from "reactstrap";
 // import "@fortawesome/fontawesome-free/css";
 
 class EditDetails extends Component {
@@ -7,11 +22,38 @@ class EditDetails extends Component {
     super(props);
     this.state = {
       account: "0x1b53a6e8d57a343de37c09833fec13f8a7fdc3b2",
+      isOpen: false,
+      name: "",
+      post: "",
     };
     this.handleClick = this.handleClick.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleClick = () => {
-    console.log("Hello");
+  handleClick = (name, post) => {
+    console.log(name, post);
+    this.props.eth
+      .removeCandidate(name, post, { from: this.props.account })
+      .then((res) => console.log(res));
+  };
+  toggleModal = (post) => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      post: post,
+    });
+  };
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  addCandidate = (post) => {
+    console.log(this.state.name, this.state.post);
+    this.props.eth
+      .addCandidate(this.state.name, this.state.post, {
+        from: this.props.account,
+      })
+      .then((res) => console.log(res));
   };
   render() {
     // console.log(this.props.account);
@@ -24,6 +66,10 @@ class EditDetails extends Component {
               <EditCandidates
                 props={this.props}
                 handleClick={this.handleClick}
+                toggle={this.toggleModal}
+                isOpen={this.state.isOpen}
+                onChange={this.handleChange}
+                addCandidate={this.addCandidate}
               />
             ) : (
               <h1>You are not authorized to edit the details</h1>
@@ -41,33 +87,126 @@ const EditCandidates = (props) => {
     <Fragment>
       <h1>Edit Candidates</h1>
       <h2>VP</h2>
-      <Candidates candidates={props.props.VP} handleClick={props.handleClick} />
+      <Candidates
+        candidates={props.props.VP}
+        handleClick={props.handleClick}
+        post={"VP"}
+        isOpen={props.isOpen}
+        toggle={props.toggle}
+        onChange={props.onChange}
+        addCandidate={props.addCandidate}
+      />
+      <Card>
+        <Button className="text-center" onClick={() => props.toggle("VP")}>
+          Add Candidate
+        </Button>
+      </Card>
       <h2>GS</h2>
-      <Candidates candidates={props.props.GS} handleClick={props.handleClick} />
+      <Candidates
+        candidates={props.props.GS}
+        handleClick={props.handleClick}
+        post={"GS"}
+        isOpen={props.isOpen}
+        toggle={props.toggle}
+        onChange={props.onChange}
+        addCandidate={props.addCandidate}
+      />
+      <Card>
+        <Button className="text-center" onClick={() => props.toggle("GS")}>
+          Add Candidate
+        </Button>
+      </Card>
       <h2>CS</h2>
-      <Candidates candidates={props.props.CS} handleClick={props.handleClick} />
+      <Candidates
+        candidates={props.props.CS}
+        handleClick={props.handleClick}
+        post={"CS"}
+        isOpen={props.isOpen}
+        toggle={props.toggle}
+        onChange={props.onChange}
+        addCandidate={props.addCandidate}
+      />
+      <Card>
+        <Button className="text-center" onClick={() => props.toggle("CS")}>
+          Add Candidate
+        </Button>
+      </Card>
       <h2>SS</h2>
-      <Candidates candidates={props.props.SS} handleClick={props.handleClick} />
+      <Candidates
+        candidates={props.props.SS}
+        handleClick={props.handleClick}
+        post={"SS"}
+        isOpen={props.isOpen}
+        toggle={props.toggle}
+        onChange={props.onChange}
+        addCandidate={props.addCandidate}
+      />
+      <Card>
+        <Button className="text-center" onClick={() => props.toggle("SS")}>
+          Add Candidate
+        </Button>
+      </Card>
+      <Modal isOpen={props.isOpen} toggle={() => props.toggle("")}>
+        <ModalHeader toggle={() => props.toggle("")}>
+          Add Candidates
+        </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup row>
+              <Label sm={4}>Candidate name </Label>
+
+              <Col sm={8}>
+                <Input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                  onChange={props.onChange}
+                />
+              </Col>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              props.addCandidate();
+              props.toggle();
+            }}
+          >
+            Add Candidate
+          </Button>{" "}
+          <Button color="secondary" onClick={() => props.toggle("")}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </Fragment>
   );
 };
 
-const Candidates = (props) =>
-  props.candidates.map((candidate) => (
-    <Card>
-      <CardBody className="text-left">
+const Candidates = (props) => (
+  <Card>
+    {props.candidates.map((candidate) => (
+      <CardHeader className="text-left">
         <div className="container">
           <div className="row">
             <h4 className="col-6">{candidate.name}</h4>
             <div className="col-6 text-right">
-              <Button outline color="secondary" onClick={props.handleClick}>
+              <Button
+                outline
+                color="secondary"
+                onClick={() => props.handleClick(candidate.name, props.post)}
+              >
                 <i className="fas fa-times-circle"></i>
               </Button>
             </div>
           </div>
         </div>
-      </CardBody>
-    </Card>
-  ));
+      </CardHeader>
+    ))}
+  </Card>
+);
 //   // <h1>Hi</h1>
 export default EditDetails;
